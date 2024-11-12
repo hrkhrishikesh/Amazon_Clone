81f74f31ed08c0525bf7c9c932a7beabf1e21a40
Hrishikesh Kumar <98553139+hrkhrishikesh@users.noreply.github.com>
2024-11-12 23:39:35 +0530

    Update api.js
    Changed api link because previous one is not working

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+//old api = "https://fakestoreapiserver.vercel.app/amazonproducts"
+
 export async function productsData() {
   const products = await axios.get(
-    "https://fakestoreapiserver.vercel.app/amazonproducts"
+    "https://retoolapi.dev/WnB1Ru/productsdata"
   );
   return products;
 }
