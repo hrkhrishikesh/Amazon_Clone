6073ea1db39417c8dd660e34c517d031d4c4b201
Hrishikesh Kumar <98553139+hrkhrishikesh@users.noreply.github.com>
2024-11-13 04:07:22 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 //old api = "https://fakestoreapiserver.vercel.app/amazonproducts"
 //demo api = "https://retoolapi.dev/WnB1Ru/productsdata"
+//api = "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
 export async function productsData() {
   const products = await axios.get(
-    "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
+    "https://retoolapi.dev/WnB1Ru/productsdata"
   );
   return products;
 }
