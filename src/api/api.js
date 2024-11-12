b92a9b7366dab30ff662111e3490ef85df29d8ab
Hrishikesh Kumar <98553139+hrkhrishikesh@users.noreply.github.com>
2024-11-13 04:04:08 +0530

    Update api.js again with demo data
    Using jsonbin.io to create a free server to store json data

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 //old api = "https://fakestoreapiserver.vercel.app/amazonproducts"
-
+//demo api = "https://retoolapi.dev/WnB1Ru/productsdata"
 export async function productsData() {
   const products = await axios.get(
-    "https://retoolapi.dev/WnB1Ru/productsdata"
+    "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
   );
   return products;
 }
