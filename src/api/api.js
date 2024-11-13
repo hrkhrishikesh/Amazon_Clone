f13838dbe89c6df94aa16de46df5128543fd2077
Hrishikesh Kumar <98553139+hrkhrishikesh@users.noreply.github.com>
2024-11-13 14:27:01 +0530

    changed products to products.data.record

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 //api = "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
 export async function productsData() {
   const products = await axios.get(
-    "https://retoolapi.dev/WnB1Ru/productsdata"
+    "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
   );
-  return products;
+  return products.data.record;
 }
