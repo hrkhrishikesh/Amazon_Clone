1eb1e8a543715fa105ac6153e462964288aaef3c
Hrishikesh Kumar <98553139+hrkhrishikesh@users.noreply.github.com>
2024-11-13 20:41:42 +0530

    cant fix rolling back.

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-import products from "./products.json";
+import product from "./products.json";
 
 //old api = "https://fakestoreapiserver.vercel.app/amazonproducts"
 //demo api = "https://retoolapi.dev/WnB1Ru/productsdata"
 //api = "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
 
 export async function productsData() {
-  // const products = await axios.get(
-  //   "https://retoolapi.dev/WnB1Ru/productsdata"
-  // );
+  const products = await axios.get(
+    "https://retoolapi.dev/WnB1Ru/productsdata"
+  );
   return products;
 }
