123aa77e9b5058f83ae4a600b0836fa772622539
hrkhrkhrk <kumar.hrishikesh27@gmail.com>
2025-05-13 01:12:59 +0530

    products images fixed

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,10 @@
-import axios from "axios";
 import product from "./products.json";
 
 //old api = "https://fakestoreapiserver.vercel.app/amazonproducts"
 //demo api = "https://retoolapi.dev/WnB1Ru/productsdata"
-// api = "https://api.jsonbin.io/v3/b/6733d76cad19ca34f8c921f8"
+
 export async function productsData() {
-  const products = await axios.get(
-    "https://retoolapi.dev/WnB1Ru/productsdata"
-  );
-  return products;
+  // Return the local products data directly
+  return { data: product };
 }
-// export async function productsData() {
-//   const response = await fetch("./products.json");
-//   return products;
-// }
+
